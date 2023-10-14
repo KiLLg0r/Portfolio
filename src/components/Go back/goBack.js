85e04b8124f002750a9703337aa37766fb86aa9a
Robert <rob.oblesniuc@gmail.com>
2023-10-14 16:43:06 +0300

    Update goBack.js

--- src/components/Go back/goBack.js
+++ src/components/Go back/goBack.js
@@ -4,11 +4,11 @@ import style from "./goBack.module.scss";
 import { useRouter } from "next/navigation";
 import { BsChevronLeft } from "react-icons/bs";
 
-const GoBack = () => {
+const GoBack = ({ to }) => {
   const router = useRouter();
 
   return (
-    <button className={style.goBack} onClick={() => router.back()}>
+    <button className={style.goBack} onClick={() => router.push(to)}>
       <BsChevronLeft />
       Go back
     </button>
